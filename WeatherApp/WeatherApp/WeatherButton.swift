99a71dc71f89916d5f2bf0e5d85427b084d30dd4
Akyl Temirgaliyev <95165544+akylmeister@users.noreply.github.com>
2024-12-05 10:21:28 +0500

    Weather App

---
+++ WeatherApp/WeatherApp/WeatherButton.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherButton.swift
+//  WeatherApp
+//
+//  Created by Akyl Temirgaliyev on 10.09.2024.
+//
+
+import SwiftUI
+
+
+struct WeatherButton: View {
+
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
+
+
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButton(title: "Test Title",
+                      textColor: .white,
+                      backgroundColor: .blue)
+    }
+}
